36e8c64adff69e351dbc57d93134d85d72ce9236
nekagit <nenad99@hotmail.de>
2024-06-03 12:52:34 +0200

    d

--- src/views/Nana/BiographyView.vue
+++ src/views/Nana/BiographyView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col p-4">
+  <div class="flex flex-col p-16">
     <h1 class="demo-title">Short Biography of Dragoslava Stankovic</h1>
     <div id="opt">
       <dl>

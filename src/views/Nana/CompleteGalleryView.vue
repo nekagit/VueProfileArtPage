789ef0e8369bc1fd7b5f89213e44a559b235ae34
nekagit <nenad99@hotmail.de>
2024-06-09 18:33:22 +0200

    d

--- src/views/Nana/CompleteGalleryView.vue
+++ src/views/Nana/CompleteGalleryView.vue
@@ -1,8 +1,10 @@
 <template>
   <AInternetFullGallery :images="ImageHelper().sellingGallery" />
+  <AMenuButton />
 </template>
 
 <script setup lang="ts">
 import AInternetFullGallery from "@/components/atoms/img/AInternetFullGallery.vue";
 import ImageHelper from "@/services/ImageHelper.ts";
+import AMenuButton from "@/components/atoms/nav/AMenuButton.vue";
 </script>

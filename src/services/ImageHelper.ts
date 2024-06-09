123c2aba8b4170e8cf9b263d02edfa559dba3a4b
nekagit <nenad99@hotmail.de>
2024-06-09 16:49:10 +0200

    d

--- src/services/ImageHelper.ts
+++ src/services/ImageHelper.ts
@@ -1,58 +1,224 @@
+import Nr1 from '@/assets/Nana/sellingGallery/1.jpeg'
+import Nr2 from '@/assets/Nana/sellingGallery/2.JPG'
+import Nr3 from '@/assets/Nana/sellingGallery/3.JPG'
+import Nr4 from '@/assets/Nana/sellingGallery/4.JPG'
+import Nr5 from '@/assets/Nana/sellingGallery/5.JPG'
+import Nr6 from '@/assets/Nana/sellingGallery/6.JPG'
+import Nr7 from '@/assets/Nana/sellingGallery/7.JPG'
+import Nr8 from '@/assets/Nana/sellingGallery/8.JPG'
+import Nr9 from '@/assets/Nana/sellingGallery/9.jpg'
+import Nr10 from '@/assets/Nana/sellingGallery/10.jpg'
+import Nr11 from '@/assets/Nana/sellingGallery/11.jpeg'
+import Nr12 from '@/assets/Nana/sellingGallery/12.jpg'
+import Nr14 from '@/assets/Nana/sellingGallery/14.jpg'
+import Nr13 from '@/assets/Nana/sellingGallery/13.jpg'
+import Nr15 from '@/assets/Nana/sellingGallery/15.jpg'
+import Nr16 from '@/assets/Nana/sellingGallery/16.jpg'
+import Nr17 from '@/assets/Nana/sellingGallery/17.jpg'
+import Nr18 from '@/assets/Nana/sellingGallery/18.jpg'
+import Nr19 from '@/assets/Nana/sellingGallery/19.jpg'
+import Nr20 from '@/assets/Nana/sellingGallery/20.JPG'
+import Nr21 from '@/assets/Nana/sellingGallery/21.jpg'
+import Nr22 from '@/assets/Nana/sellingGallery/22.jpg'
+import Nr23 from '@/assets/Nana/sellingGallery/23.jpg'
+import Nr24 from '@/assets/Nana/sellingGallery/24.jpg'
+import Nr25 from '@/assets/Nana/sellingGallery/25.jpg'
+import Nr26 from '@/assets/Nana/sellingGallery/26.jpg'
+import Nr27 from '@/assets/Nana/sellingGallery/27.jpg'
+import Nr28 from '@/assets/Nana/sellingGallery/28.jpg'
+import Nr29 from '@/assets/Nana/sellingGallery/29.jpg'
+import Nr30 from '@/assets/Nana/sellingGallery/30.jpg'
+import Nr31 from '@/assets/Nana/sellingGallery/31.jpg'
+import Nr32 from '@/assets/Nana/sellingGallery/32.jpg'
+import Nr33 from '@/assets/Nana/sellingGallery/33.jpg'
+import Nr34 from '@/assets/Nana/sellingGallery/34.jpg'
+import Nr36 from '@/assets/Nana/sellingGallery/36.jpg'
+import Nr37 from '@/assets/Nana/sellingGallery/37.jpg'
+import Nr38 from '@/assets/Nana/sellingGallery/38.jpg'
+import Nr39 from '@/assets/Nana/sellingGallery/39.jpg'
+import Nr40 from '@/assets/Nana/sellingGallery/40.jpg'
+import Nr41 from '@/assets/Nana/sellingGallery/41.jpg'
+import Nr42 from '@/assets/Nana/sellingGallery/42.jpg'
+import Nr43 from '@/assets/Nana/sellingGallery/43.jpg'
+import Nr44 from '@/assets/Nana/sellingGallery/44.jpg'
+import Nr45 from '@/assets/Nana/sellingGallery/45.jpg'
+import Nr46 from '@/assets/Nana/sellingGallery/46.jpg'
+import Nr47 from '@/assets/Nana/sellingGallery/47.jpg'
+import Nr48 from '@/assets/Nana/sellingGallery/48.jpg'
+import Nr49 from '@/assets/Nana/sellingGallery/49.jpg'
+import Nr50 from '@/assets/Nana/sellingGallery/50.jpg'
+import Nr51 from '@/assets/Nana/sellingGallery/51.jpg'
+import Nr52 from '@/assets/Nana/sellingGallery/52.jpg'
+import Nr53 from '@/assets/Nana/sellingGallery/53.jpg'
+import Nr54 from '@/assets/Nana/sellingGallery/54.jpg'
+import Nr55 from '@/assets/Nana/sellingGallery/55.jpg'
+import Nr56 from '@/assets/Nana/sellingGallery/56.jpg'
+import Nr57 from '@/assets/Nana/sellingGallery/57.jpg'
+import Nr58 from '@/assets/Nana/sellingGallery/58.jpg'
+import Nr59 from '@/assets/Nana/sellingGallery/59.jpg'
+import Nr60 from '@/assets/Nana/sellingGallery/60.jpg'
+import Nr61 from '@/assets/Nana/sellingGallery/61.jpg'
+import Nr62 from '@/assets/Nana/sellingGallery/62.jpg'
+import Nr63 from '@/assets/Nana/sellingGallery/63.JPG'
+import Nr66 from '@/assets/Nana/sellingGallery/66.jpg'
+import Nr67 from '@/assets/Nana/sellingGallery/67.jpg'
+import Nr68 from '@/assets/Nana/sellingGallery/68.jpg'
+import Nr69 from '@/assets/Nana/sellingGallery/69.jpg'
+import Nr70 from '@/assets/Nana/sellingGallery/70.jpg'
+import Nr71 from '@/assets/Nana/sellingGallery/71.jpeg'
+import Nr72 from '@/assets/Nana/sellingGallery/72.jpeg'
+import Nr73 from '@/assets/Nana/sellingGallery/73.jpeg'
+import Nr74 from '@/assets/Nana/sellingGallery/74.jpeg'
+import Nr75 from '@/assets/Nana/sellingGallery/75.jpg'
+import Nr76 from '@/assets/Nana/sellingGallery/76.jpg'
+import Nr77 from '@/assets/Nana/sellingGallery/77.jpeg'
+import Nr78 from '@/assets/Nana/sellingGallery/78.jpg'
+import Nr79 from '@/assets/Nana/sellingGallery/79.jpg'
+import Nr80 from '@/assets/Nana/sellingGallery/80.jpg'
+import Nr81 from '@/assets/Nana/sellingGallery/81.jpg'
+import Nr82 from '@/assets/Nana/sellingGallery/82.jpg'
+import Nr83 from '@/assets/Nana/sellingGallery/83.jpg'
+import Nr84 from '@/assets/Nana/sellingGallery/84.jpg'
+import Nr85 from '@/assets/Nana/sellingGallery/85.jpg'
+import Nr86 from '@/assets/Nana/sellingGallery/86.jpg'
+import Nr87 from '@/assets/Nana/sellingGallery/87.jpg'
+import Nr88 from '@/assets/Nana/sellingGallery/88.jpg'
+import Nr89 from '@/assets/Nana/sellingGallery/89.jpg'
+import Nr90 from '@/assets/Nana/sellingGallery/90.jpg'
+import Nr91 from '@/assets/Nana/sellingGallery/91.jpg'
+import Nr92 from '@/assets/Nana/sellingGallery/92.jpg'
+import Nr93 from '@/assets/Nana/sellingGallery/93.jpg'
+import Nr94 from '@/assets/Nana/sellingGallery/94.jpg'
+import Nr95 from '@/assets/Nana/sellingGallery/95.jpg'
+import Nr96 from '@/assets/Nana/sellingGallery/96.jpg'
+import Nr97 from '@/assets/Nana/sellingGallery/97.jpg'
+import Nr98 from '@/assets/Nana/sellingGallery/98.jpg'
+import Nr99 from '@/assets/Nana/sellingGallery/99.jpg'
+import Nr100 from '@/assets/Nana/sellingGallery/100.jpg'
+import Nr101 from '@/assets/Nana/sellingGallery/101.jpg'
+import Nr102 from '@/assets/Nana/sellingGallery/102.jpg'
+
 import Intro2 from '@/assets/Nana/introGallery/2.jpg'
 import Intro3 from '@/assets/Nana/introGallery/3.JPG'
 import Intro4 from '@/assets/Nana/introGallery/4.jpg'
-import NanaProfil from '@/assets/Nana/introGallery/WhatsApp Bild 2024-06-08 um 11.57.53_3f6ca3e5.jpg'
-
-import spring1 from '@/assets/Nana/springGallery/1.JPG'
-import spring2 from '@/assets/Nana/springGallery/2.JPG'
-import spring3 from '@/assets/Nana/springGallery/3.jpg'
-
-import summer1 from '@/assets/Nana/summerGallery/1.jpg'
-import summer2 from '@/assets/Nana/summerGallery/2.jpg'
-import summer3 from '@/assets/Nana/summerGallery/3.jpg'
-import summer4 from '@/assets/Nana/summerGallery/4.jpg'
-
-import autmn1 from '@/assets/Nana/autmnGallery/1.JPG'
-import autmn2 from '@/assets/Nana/autmnGallery/2.JPG'
-import autmn3 from '@/assets/Nana/autmnGallery/3.JPG'
-import autmn4 from '@/assets/Nana/autmnGallery/4.jpeg'
-
-import winter1 from '@/assets/Nana/winterGallery/1.JPG'
-import winter2 from '@/assets/Nana/winterGallery/2.jpg'
-import winter3 from '@/assets/Nana/winterGallery/3.jpg'
-import winter4 from '@/assets/Nana/winterGallery/4.jpg'
-
-import abstract1 from '@/assets/Nana/abstractGallery/1.jpg'
-import abstract2 from '@/assets/Nana/abstractGallery/2.jpg'
-import abstract3 from '@/assets/Nana/abstractGallery/3.jpg'
-import abstract4 from '@/assets/Nana/abstractGallery/4.jpg'
-
-import other1 from '@/assets/Nana/otherGallery/1.JPG'
-import other2 from '@/assets/Nana/otherGallery/2.jpg'
-import other3 from '@/assets/Nana/otherGallery/3.jpg'
-import other4 from '@/assets/Nana/otherGallery/4.jpg'
+import NanaProfil from "@/assets/Nana/introGallery/WhatsApp Bild 2024-06-08 um 11.57.53_3f6ca3e5.jpg"
 
 import logo from '@/assets/Nana/introGallery/logo.png'
 import ProfilePic from '@/assets/Nana/introGallery/profilePic.jpg'
 export default function ImageHelper() {
+  const sellingGallery = [
+    { src: Nr1, title: '#1' },
+    { src: Nr2, title: '#2' },
+    { src: Nr3, title: '#3' },
+    { src: Nr4, title: '#4' },
+    { src: Nr5, title: '#5' },
+    { src: Nr6, title: '#6' },
+    { src: Nr7, title: '#7' },
+    { src: Nr8, title: '#8' },
+    { src: Nr9, title: '#9' },
+    { src: Nr10, title: '#10' },
+    { src: Nr11, title: '#11' },
+    { src: Nr12, title: '#12' },
+    { src: Nr13, title: '#13' },
+    { src: Nr14, title: '#14' },
+    { src: Nr15, title: '#15' },
+    { src: Nr16, title: '#16' },
+    { src: Nr17, title: '#17' },
+    { src: Nr18, title: '#18' },
+    { src: Nr19, title: '#19' },
+    { src: Nr20, title: '#20' },
+    { src: Nr21, title: '#21' },
+    { src: Nr22, title: '#22' },
+    { src: Nr23, title: '#23' },
+    { src: Nr24, title: '#24' },
+    { src: Nr25, title: '#25' },
+    { src: Nr26, title: '#26' },
+    { src: Nr27, title: '#27' },
+    { src: Nr28, title: '#28' },
+    { src: Nr29, title: '#29' },
+    { src: Nr30, title: '#30' },
+    { src: Nr31, title: '#31' },
+    { src: Nr32, title: '#32' },
+    { src: Nr33, title: '#33' },
+    { src: Nr34, title: '#34' },
+    { src: Nr36, title: '#36' },
+    { src: Nr37, title: '#37' },
+    { src: Nr38, title: '#38' },
+    { src: Nr39, title: '#39' },
+    { src: Nr40, title: '#40' },
+    { src: Nr41, title: '#41' },
+    { src: Nr42, title: '#42' },
+    { src: Nr43, title: '#43' },
+    { src: Nr44, title: '#44' },
+    { src: Nr45, title: '#45' },
+    { src: Nr46, title: '#46' },
+    { src: Nr47, title: '#47' },
+    { src: Nr48, title: '#48' },
+    { src: Nr49, title: '#49' },
+    { src: Nr50, title: '#50' },
+    { src: Nr51, title: '#51' },
+    { src: Nr52, title: '#52' },
+    { src: Nr53, title: '#53' },
+    { src: Nr54, title: '#54' },
+    { src: Nr55, title: '#55' },
+    { src: Nr56, title: '#56' },
+    { src: Nr57, title: '#57' },
+    { src: Nr58, title: '#58' },
+    { src: Nr59, title: '#59' },
+    { src: Nr60, title: '#60' },
+    { src: Nr61, title: '#61' },
+    { src: Nr62, title: '#62' },
+    { src: Nr63, title: '#63' },
+    { src: Nr66, title: '#66' },
+    { src: Nr67, title: '#67' },
+    { src: Nr68, title: '#68' },
+    { src: Nr69, title: '#69' },
+    { src: Nr70, title: '#70' },
+    { src: Nr71, title: '#71' },
+    { src: Nr72, title: '#72' },
+    { src: Nr73, title: '#73' },
+    { src: Nr74, title: '#74' },
+    { src: Nr75, title: '#75' },
+    { src: Nr76, title: '#76' },
+    { src: Nr77, title: '#77' },
+    { src: Nr78, title: '#78' },
+    { src: Nr79, title: '#79' },
+    { src: Nr80, title: '#80' },
+    { src: Nr81, title: '#81' },
+    { src: Nr82, title: '#82' },
+    { src: Nr83, title: '#83' },
+    { src: Nr84, title: '#84' },
+    { src: Nr85, title: '#85' },
+    { src: Nr86, title: '#86' },
+    { src: Nr87, title: '#87' },
+    { src: Nr88, title: '#88' },
+    { src: Nr89, title: '#89' },
+    { src: Nr90, title: '#90' },
+    { src: Nr91, title: '#91' },
+    { src: Nr92, title: '#92' },
+    { src: Nr93, title: '#93' },
+    { src: Nr94, title: '#94' },
+    { src: Nr95, title: '#95' },
+    { src: Nr96, title: '#96' },
+    { src: Nr97, title: '#97' },
+    { src: Nr98, title: '#98' },
+    { src: Nr99, title: '#99' },
+    { src: Nr100, title: '#100' },
+    { src: Nr101, title: '#101' },
+    { src: Nr102, title: '#102' }
+];
+
   const introGallery = [
-    { src: Intro2, title: '#1' },
-    { src: Intro3, title: '#1' },
-    { src: Intro4, title: '#1' },
-    { src: abstract1, title: '#1' },
-    { src: spring2, title: '#1' },
-    { src: summer1, title: '#1' },
-    { src: winter1, title: '#1' },
-    { src: abstract2, title: '#1' }
+    { src: Nr82, title: '#82' },
+    { src: Nr83, title: '#83' },
+    { src: Nr84, title: '#84' },
+    { src: Nr85, title: '#85' },
+    { src: Nr86, title: '#86' },
+    { src: Nr87, title: '#87' },
+    { src: Nr88, title: '#88' },
+    { src: Nr89, title: '#89' }
   ]
   const exhibitionGallery = [
-    { src: Intro2, title: '#1' },
-    { src: Intro3, title: '#1' },
-    { src: Intro4, title: '#1' },
-    { src: ProfilePic, title: '#1' },
-    { src: logo, title: '#1' }
-  ]
-  const artworkGallery = [
     { src: Intro2, title: '#1' },
     { src: Intro3, title: '#1' },
     { src: Intro4, title: '#1' },
@@ -67,39 +233,48 @@ export default function ImageHelper() {
     { src: logo, title: '#1' }
   ]
   const springGallery = [
-    { src: spring1, title: '#1' },
-    { src: spring2, title: '#1' },
-    { src: spring3, title: '#1' }
+    { src: Nr1, title: '#1' },
+    { src: Nr26, title: '#26' },
+    { src: Nr29, title: '#29' },
+    { src: Nr30, title: '#30' },
+    { src: Nr30, title: '#30' },
+    { src: Nr34, title: '#34' },
   ]
   const summerGallery = [
-    { src: summer1, title: '#1' },
-    { src: summer2, title: '#1' },
-    { src: summer3, title: '#1' },
-    { src: summer4, title: '#1' }
+    { src: Nr4, title: '#4' },
+    { src: Nr40, title: '#40' },
+    { src: Nr48, title: '#48' },
+    { src: Nr54, title: '#54' },
+    { src: Nr58, title: '#58' },
   ]
   const autmnGallery = [
-    { src: autmn1, title: '#1' },
-    { src: autmn2, title: '#1' },
-    { src: autmn3, title: '#1' },
-    { src: autmn4, title: '#1' }
+    { src: Nr2, title: '#2' },
+    { src: Nr8, title: '#8' },
+    { src: Nr12, title: '#12' },
+    { src: Nr18, title: '#18' },
+    { src: Nr19, title: '#19' },
+    { src: Nr27, title: '#27' },
   ]
   const winterGallery = [
-    { src: winter1, title: '#1' },
-    { src: winter2, title: '#1' },
-    { src: winter3, title: '#1' },
-    { src: winter4, title: '#1' }
+    { src: Nr7, title: '#7' },
+    { src: Nr30, title: '#30' },
+    { src: Nr37, title: '#37' },
+    { src: Nr39, title: '#39' },
+    { src: Nr47, title: '#47' }
   ]
   const abstractGallery = [
-    { src: abstract1, title: '#1' },
-    { src: abstract2, title: '#1' },
-    { src: abstract3, title: '#1' },
-    { src: abstract4, title: '#1' }
+    { src: Nr61, title: '#61' },
+    { src: Nr63, title: '#63' },
+    { src: Nr67, title: '#67' },
+    { src: Nr70, title: '#70' },
+    { src: Nr71, title: '#71' },
   ]
   const otherGallery = [
-    { src: other1, title: '#1' },
-    { src: other2, title: '#1' },
-    { src: other3, title: '#1' },
-    { src: other4, title: '#1' }
+    { src: Nr60, title: '#60' },
+    { src: Nr76, title: '#76' },
+    { src: Nr77, title: '#77' },
+    { src: Nr78, title: '#78' },
+    { src: Nr86, title: '#86' },
   ]
   const artPage = [
     springGallery,
@@ -110,11 +285,10 @@ export default function ImageHelper() {
     otherGallery
   ]
   return {
-
+    sellingGallery,
     introGallery,
     artPage,
     exhibitionGallery,
-    artworkGallery,
     lifeGallery,
     springGallery,
     summerGallery,
